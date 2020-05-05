2f21ead0d85543fae552ce1d2f02f3fd68bfde26
Anthony Boffman <amarrokalboffman@gmail.com>
2020-05-05 15:54:39 -0700

    Add service worker and adjust manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,71 @@
 // TODO: implement service worker so that users can use the app offline. The SW
 // will need to cache static assets to display the app offline. Additionally,
+const FILES_TO_CACHE = [
+  "/",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "/index.html",
+  "/index.js",
+  "/db.js",
+  "/icons/icon-192x169.png",
+  "/icons/icon-512x452.png",
+  "manifest.webmanifest",
+  "/styles.css"
+];
+
 // the SW should cache transaction data, using the cached data as a fallback
 // when the app is used offline. HINT: You should use two caches. One for the
-// static assets such ass html, css, js, images, etc; and another cache for
-// the dynamic data from requests to routes beginning with "/api".
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .catch((err) => console.log(err))
+      .then(self.skipWaiting())
+  );
+});
+
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(() => {
+            return cache.match(event.request);
+          });
+      })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+})
